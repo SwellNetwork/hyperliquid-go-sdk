e2b001b9c4257737d813d47661892ea36403d60d
Harry <harry@swellnetwork.io>
2025-11-13 10:11:26 +0700

    fix: lint

--- interfaces.go
+++ interfaces.go
@@ -6,6 +6,5 @@ type InfoClient interface {
 	PredictedFundings(ctx context.Context) ([]PredictedFunding, error)
 	FundingHistory(ctx context.Context, params FundingHistoryParams) ([]FundingHistory, error)
 	MetaAndAssetCtxs(ctx context.Context) (*MetaAndAssetCtx, error)
-	
 	SpotMetaAndAssetCtxs(ctx context.Context) (*SpotMetaAndAssetCtx, error)
 }
